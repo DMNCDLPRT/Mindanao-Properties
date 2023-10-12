853a253780dee375190ce5d590d96070995103e8
bug-dbg <90324449+bug-dbg@users.noreply.github.com>
2023-10-12 09:09:12 +0800

    fix: image to render in properties and details

--- app/Livewire/Components/ImageModal.php
+++ app/Livewire/Components/ImageModal.php
@@ -3,9 +3,19 @@
 namespace App\Livewire\Components;
 
 use Livewire\Component;
+use App\Models\Property;
 
 class ImageModal extends Component
 {
+
+    public $property;
+
+    public function mount(Property $property)
+    {
+       
+        $this->property = $property;
+    }
+
     public function render()
     {
         return view('livewire.components.image-modal');
